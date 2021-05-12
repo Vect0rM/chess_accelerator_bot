2f87e4aebcd8c15b5f50760bd553dbc4a007f106
Misha <m.skvortsovvv@gmail.com>
2021-05-12 18:11:45 +0300

    1

--- src/main/java/Chess_bot.java
+++ src/main/java/Chess_bot.java
@@ -1,2 +1,60 @@
-public class Chess_bot {
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Chess_bot extends TelegramLongPollingBot {
+    public  void SetButtons(SendMessage sendMessage){
+        ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
+        sendMessage.setReplyMarkup(replyKeyboardMarkup);
+        replyKeyboardMarkup.setResizeKeyboard(true);
+        replyKeyboardMarkup.setSelective(true);
+        replyKeyboardMarkup.setOneTimeKeyboard(true);
+
+        List<KeyboardRow> keyboardRowList = new ArrayList<>();
+        KeyboardRow keyboardRow1 = new KeyboardRow();
+        keyboardRow1.add(new KeyboardButton("Кто Я?"));
+        keyboardRowList.add(keyboardRow1);
+        replyKeyboardMarkup.setKeyboard(keyboardRowList);
+    }
+
+    public void SendMsg(Message message, String text) {
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.enableMarkdown(true);
+        sendMessage.setChatId(message.getChatId().toString());
+     // sendMessage.setReplyToMessageId(message.getMessageId()); - Ответ на сообщ
+        sendMessage.setText(text);
+        try {
+            SetButtons(sendMessage);
+            execute(sendMessage);
+        } catch (TelegramApiException e){
+            e.printStackTrace();
+        }
+    }
+    @Override
+    public void onUpdateReceived(Update update) {
+        Message message = update.getMessage();
+        if (message != null && message.hasText()) {
+            switch (message.getText()) {
+                case "/help" -> SendMsg(message, "Чем могу быть полезен?");
+                case "Привет" -> SendMsg(message, "Здарова!");
+            }
+        }
+    }
+    @Override
+    public String getBotUsername() {
+        return "chess_accelerator_bot";
+    }
+
+    @Override
+    public String getBotToken() {
+        return "1834908590:AAGm6ybJNjUjyqKeu7EJCoJox8B_rjWjgFI";
+    }
 }
